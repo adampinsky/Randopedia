924ba62509cd796b00606f1294ae33a8747eed70
Adam Pinsky <adam.pinsky4@gmail.com>
2016-06-23 12:50:31 -0500

    finalize

--- js/script.js
+++ js/script.js
@@ -1,13 +1,58 @@
 $(document).ready(function() {
-    function revealSearchBox() {
-        $('#search-box').animate({width: '60%'});
-        $('#search-submit').animate({});
-    }
 
-    function hideSearchBox() {
-        $('#search-box').animate({width: '0'});
-    }
+    //------------//
+    // SEARCH BAR //
+    //------------//
+    $('#search-label').on('click', function() {
 
-    $('#search-label').mouseenter(revealSearchBox);
-    $('#search-label').mouseleave(hideSearchBox);
+        $('#search-box').focus();
+        return false;
+    });
+    
+    $('#search-label').mouseover(function() {
+        $('#search-box').stop().animate({
+            width : '500px',
+            paddingLeft : '5px',
+            borderWidth : '3px',
+            borderStyle : 'solid',
+            borderColor : '#373A3C'
+        }, 'fast');
+        $('#search-bar').animate({
+            left : '-50px'
+        }, 'fast');
+    });
+    
+    $('#search-label').mouseout(function() {
+        if(!$('#search-box').is(':focus')) {
+            $('#search-box').stop().animate({
+                width : '0px',
+                border : 'none',
+                padding : '0'
+            }, 'fast');
+            $('#search-bar').animate({
+            left : '0'
+        }, 'fast');
+        }
+    });
+        
+    //---------------//
+    // Submit Button //
+    //---------------//
+    $('#search-submit').on('click', function() {
+        var searchTerm = $('#search-box').val();
+        
+        if(searchTerm.length > 0) {
+            var apiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + searchTerm + '&format=json&callback=wikiCallback';
+            
+            $.ajax({
+                url :  apiUrl,
+                dataType : 'jsonp',
+                success : function(response) {
+                    window.open('https://en.wikipedia.org/wiki/' + response[1][0]);
+                }
+            });
+        } else {
+            window.open('https://en.wikipedia.org/wiki/Special:Random');
+        }
+    });
 });
